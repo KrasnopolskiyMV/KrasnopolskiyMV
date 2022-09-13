764760e8c0a4e66f248f07efbd08e60834b6ac4c
KrasnopolskiyMV <113310923+KrasnopolskiyMV@users.noreply.github.com>
2022-09-13 22:17:19 +0300

    Delete Задания к занятию 1 directory

--- Задания к занятию 1/task 6.py
+++
@@ -1,6 +0,0 @@
-# -- coding: utf-8 --
-x = input()
-y = input()
-x, y = y, x
-print(x)
-print(y)
--- Задания к занятию 1/task 7.py
+++
@@ -1,6 +0,0 @@
-# -- coding: utf-8 --
-number = int(input())
-if number % 2 == 0:
-    print('Число чётное')
-else:
-    print('Число нечётное')
