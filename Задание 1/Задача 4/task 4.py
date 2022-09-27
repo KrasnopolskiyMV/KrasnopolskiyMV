fac3865f9057da6075ef3230271afffea029bcff
KrasnopolskiyMV <113310923+KrasnopolskiyMV@users.noreply.github.com>
2022-09-27 19:50:01 +0300

    Delete task 4.py

--- Задание 1/Задача 4/task 4.py
+++
@@ -1,6 +0,0 @@
-# -- coding: utf-8 --
-seconds = int(input('Введите кол-во секунд: '))
-days = seconds // (3600 * 24)
-hours = seconds // 3600
-minutes = seconds // 60
-print(str(days) + ':' + str(hours - days*24) + ':' + str(minutes - hours*60) + ':' + str(seconds - minutes*60))
