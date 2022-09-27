12c40478143a8678a215b136e65fa16b164ba2d5
KrasnopolskiyMV <113310923+KrasnopolskiyMV@users.noreply.github.com>
2022-09-27 19:37:21 +0300

    Add files via upload

--- Задание 3/Задача 5/Задание 3.5.py
+++ Задание 3/Задача 5/Задание 3.5.py
@@ -1,6 +1,11 @@
 # -- coding: utf-8 --
 def less_number(a, b, c):
-    return(min(a, b, c))
+    if a < b and b < c:
+        return a
+    elif b < a and b < c:
+        return b
+    elif c < a and c < b:
+        return c
 first = int(input('Введите первое число: '))
 second = int(input('Введите второе число: '))
 third = int(input('Введите третье число: '))
